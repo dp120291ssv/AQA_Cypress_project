80fafc89208ae2593cd1bff2b024e00e94164310
dn120291ssv1 <Smnv0969867628>
2020-12-11 10:56:49 +0200

    add example with Contains

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,5 +1,26 @@
 /// <reference types="Cypress" />
 
+it('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('Sign in');
+})
+
+
+it('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('div', 'Sign in');
+})
+
+it('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('SIGN IN', {matchCase: false});
+})
+
+it.only('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('footer').contains('Go to old version');
+})
+
 
 
 
