debecafde32ec9eaaeed7cdfd1e2211722570724
dn120291ssv1 <Smnv0969867628>
2020-12-29 12:24:15 +0200

    add e2e, mock & stub, api tests

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -12,7 +12,7 @@ beforeEach("setuo success response with stub", () => {
 });
 
 //Example stub api success response: state of archive: a, b, e
-it.skip("check success state of payment in the archive | public session", () => {
+it("check success state of payment in the archive | public session", () => {
   cy.intercept("https://next.privat24.ua/api/p24/pub/archive", {
     fixture: "archiveResponse/success.json",
   });
@@ -30,7 +30,7 @@ it("check error state of payment in the archive | public session", () => {
 });
 
 //Example mock thank you screen in mobile replenishment proccess
-it.skip("Replenishment of Ukrainian mobile phone number", () => {
+it("Replenishment of Ukrainian mobile phone number", () => {
   basePage.open("https://next.privat24.ua/mobile?lang=en");
   mobileReplenishment.typePhoneNumber("686979712");
   basePage.typeAmount("1");
@@ -46,7 +46,7 @@ it.skip("Replenishment of Ukrainian mobile phone number", () => {
 });
 
 //Example e2e test for transfers
-it.skip("Money transfer between foreign cards", () => {
+it("Money transfer between foreign cards", () => {
   basePage.open("https://next.privat24.ua/money-transfer/card?lang=en");
   basePage.typeDebitCardData("4552331448138217", "0524", "111");
   transfers.typeDebitNameAndSurname("Shayne", "McConnell");
@@ -61,4 +61,47 @@ it.skip("Money transfer between foreign cards", () => {
   transfers.checkDebitComission("89.33 UAH");
   transfers.checktotalCurrency("UAH");
   transfers.checkComment("Cypress test");
+});
+
+//Example HTTP GET request
+it("Example sending the GET request", () => {
+  cy.request("https://next.privat24.ua").then((response) => {
+    console.log(response);
+  });
+});
+
+//Example HTTP POST request with expect verification of response
+it("Example sending the POST request", () => {
+  const requestBody = {
+    action: "info",
+    phone: "+380686979712",
+    amount: 50,
+    currency: "UAH",
+    cardCvv: "111",
+    card: "4552331448138217",
+    cardExp: "0526",
+    xref: "08ff0ad086fe9e4c5ab2e88a4bcee171",
+    _: 1609237038419,
+  };
+
+  const headersData = {
+    cookie:
+      "_ga=GA1.2.1276086127.1608750658; _gid=GA1.2.687351041.1609060591; pubkey=964d8434883ba595c7170ec0f1fb503a; fp=18; lfp=12/23/2020, 9:11:08 PM",
+  };
+
+  cy.request({
+    method: "POST",
+    url: "https://next.privat24.ua/api/p24/pub/mobipay",
+    body: requestBody,
+    headers: headersData,
+  }).then((response) => {
+    expect(response).to.have.property("status").to.equal(200);
+    expect(response.requestHeaders)
+      .to.have.property("cookie")
+      .not.be.oneOf(["", null]);
+    expect(response.body).to.have.property("status").to.equal("success");
+    expect(response.body.data).to.have.property("amount").to.equal("50.0");
+    expect(response.body.data).to.have.property("status").to.equal("ok");
+    console.log(response.body);
+  });
 });
